62fec5410ffc07d363bdc6cd53c4f98cb347798d
sassa <samuelsouto21@gmail.com>
2025-05-27 09:57:54 -0300

    Ataluização pasta public

--- public/index.js
+++ public/index.js
@@ -1,46 +1,18 @@
 require("dotenv").config();
+const Mail = require("./mail");
 
 const express = require("express");
 const lermail = require("nodemailer");
 
-
-function sendMailLer( hst , port , secure , user , pass , mailTo  , titulo , msgHTML , msgText ){
-
-    const transporter = nodemailer.createTransport({
-
-    	host : hst ,
-    	prot : port ,
-    	secure : secure ,
-    	auth : {
-    		user : user,
-    		pass : pass,
-    	},
-	});
-
-	transporter.sendMail({
-		from :  titulo ,
-		to : mailTo ,
-		subject : titulo ,
-		html : msgHTML  ,
-		text : msgText ,
-		// attachments : [
-		// 	{
-		// 		filename : "meu pdf.pdf",
-		// 		path : "caminho"
-		// 	}
-		// ]
-
-	}).then((resposne) => console.log(`Disparo realizado com sucesso  ${user} : ${resposne.messageId}`))
-	.catch((err) => { console.log(`Erro ${err} ao disparar email ${user}`)})
-}
 
 
 const app = express();
 app.use(express.json());
 
 app.get("/mail/enviar", async (req, res) => {
-   return res.json({ mesage: `ola mundo ${ nome}` });
+   return res.json({ mesage: `ola mundo ` });
 });
+
 
 app.post( "/mail/sendMain" , async (req , res ) =>{
 	const {  hst , port , secure , user , pass , mailTo  , titulo , msgHTML , msgText } = req.body ; 
@@ -50,15 +22,16 @@ app.post( "/mail/sendMain" , async (req , res ) =>{
 	}
 
 	try{
-		sendMailLer(hst , port , secure , user , pass , mailTo  , titulo , msgHTML , msgText);
-		return res.status(200).json({ mesage : `contaudo enviado com sucesso `})
+		
+		let retorno  = lermail.sendMailLerSimple(hst , port , secure , user , pass , mailTo  , titulo , msgHTML , msgText);
+
+		return res.status(200).json({ mesage : retorno });
 	}catch(err){
 		return res.status(500).json({ mesage : `Erro na execução do processo ${err.message}`});
 	}
-
-
 })
+
 
 app.listen(3000, () => {
   console.log("Servidor ativo na porta 3000");
-});
+}); 
