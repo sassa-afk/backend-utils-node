81765ef2174da13e20da7f66c000eafc2bdc406e
sassa <samuelsouto21@gmail.com>
2025-05-27 13:27:04 -0300

    ajuste data e hora

--- public/Default.js
+++ public/Default.js
@@ -1,20 +1,16 @@
 class Default {
-  dateFormat() {
-    const data = new Date();
-    const op = {
-      timeZone: "America/Sao_Paulo",
-      day: "2-digit",
-      month: "2-digit",
-      year: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-    };
+ 
+	dateFormat() {
+	  const date = new Date();
+	  const day = String(date.getDate()).padStart(2, "0");
+	  const month = String(date.getMonth() + 1).padStart(2, "0"); // meses começam do 0
+	  const year = date.getFullYear();
+	  const hours = String(date.getHours()).padStart(2, "0");
+	  const minutes = String(date.getMinutes()).padStart(2, "0");
+	  const seconds = String(date.getSeconds()).padStart(2, "0");
 
-    const dataFormatada = new Intl.DateTimeFormat("pt-BR", op).format(data);
-
-    return dataFormatada;
-  }
+	  return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+	}
 }
 
 module.exports = Default;
