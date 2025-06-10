4c7c9b21b696f568907aacc24af9de0296d8b152
sassa <samuelsouto21@gmail.com>
2025-06-10 11:47:13 -0300

    ajust

--- public/index.js
+++ public/index.js
@@ -157,7 +157,7 @@ app.patch("/google/calender/edtEvent", async (req, res) => {
 
   } catch (er) {
     def.logs(false, `At ${def.dateFormat()} : ${er}`);
-    return res.status(500).json({ mesage: `At ${def.dateFormat()} , ${er}` });
+    return res.status(500).json({ mesage: `At ${def.dateFormat()} , ${er.message}` });
   }
 });
 
