dc6587cf69fc5657c1b72a497cb7a66777dfad19
sassa <samuelsouto21@gmail.com>
2025-06-12 13:15:22 -0300

    ajuste final

--- public/OCRSpace.js
+++ public/OCRSpace.js
@@ -5,14 +5,14 @@ const fs = require('fs');
 const axios = require('axios');
 const path = require('path');
 const FormData = require('form-data');
-
- 
+const mime = require('mime-types');
 
 class OCRSpace extends Default {
 	
 	 async describle  (token , fileimg  ){
 	 	 try{
 
+			const contentType = mime.lookup(imagePath) || 'application/octet-stream';
 	 	 	const imagePath = path.resolve(fileimg);
 	 	 	const form = new FormData();
 	 	 	
@@ -21,11 +21,15 @@ class OCRSpace extends Default {
 		    form.append('isOverlayRequired', 'true');
 		    form.append('detectOrientation', 'true');
 		    form.append('OCREngine', '2');
-		    form.append('file', fs.createReadStream(imagePath) ,
-		    	{
-		    	filename: path.basename(imagePath),
-        		contentType: 'image/png' 
-		    });
+		    // form.append('file', fs.createReadStream(imagePath) ,
+		    // 	{
+		    // 	filename: path.basename(imagePath),
+        	// 	contentType: 'image/png' 
+		    // });
+		    form.append('file', fs.createReadStream(imagePath), {
+			  filename: path.basename(imagePath),
+			  contentType: contentType
+			});
 
 		    const retorno = await axios.post(
 		    	'https://api.ocr.space/parse/image',
